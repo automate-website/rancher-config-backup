e5ccca9bb785f2737951b2614bf18ad2bfdc47c5
build-failure <michael@lewkowski.de>
2019-01-01 18:12:18 +0100

    Add travis-ci config and property defaults

--- src/main/java/website/automate/rancher/configbackup/props/ConfigBackupProps.java
+++ src/main/java/website/automate/rancher/configbackup/props/ConfigBackupProps.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "rancher-config-backup")
+@ConfigurationProperties
 public class ConfigBackupProps {
 
     private String gitUserName;
